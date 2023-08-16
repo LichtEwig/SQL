0008594035ad219c8699686f729f36b8a8f0d090
LichtEwig <lichtewig73@gmail.com>
2023-08-16 16:11:16 -0300

    base de datos 1

---
+++ sql_ddl/base_datos.sql
@@ -0,0 +1,9 @@
+-- creando la base de datos escuelas 
+CREATE DATABASE escuelas; 
+
+-- le tengo que indicar la base de datos a utilizar
+-- USE nombre_base_datos
+USE escuelas;
+
+-- creando una tabla
+CREATE TABLE alumnos (id_alumno INTEGER, nombre VARCHAR(30));
