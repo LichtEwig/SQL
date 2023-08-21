eb11b03dfb5321c20b9801038b2012856995bb7e
LichtEwig <lichtewig73@gmail.com>
2023-08-20 22:27:57 -0300

    foreign key

--- sql_2/db_23679.sql
+++ sql_2/db_23679.sql
@@ -3,58 +3,62 @@
 de lineas
 multiples */
 
-USE db_23679;
+--USE db_23679;
 /* CREATE DATABASE db_23679;  */
 
 -- creando la tabla escuelas con Clave Primaria id_escuela
-CREATE TABLE escuelas(
+/* CREATE TABLE escuelas(
     id_escuela INT,
     nombre VARCHAR(30),
     localidad VARCHAR(30),
     provincia VARCHAR(40),
-    capacidad INT, 
+     capacidad INT, 
     PRIMARY KEY(id_escuela)
-);
+); */ 
 
 /* CREATE TABLE prueba(
     id INT
-);
- */
+); */
+
 
 -- eliminando la tabla prueba
--- DROP TABLE prueba;
+ /* DROP TABLE prueba; */
 
 -- agregando una columna
--- ALTER TABLE escuelas ADD columna_nueva INT;
+/* ALTER TABLE escuelas ADD columna_nueva INT; */
 
 -- eliminando una columna
--- ALTER TABLE escuelas DROP columna_nueva;
+ /* ALTER TABLE escuelas DROP columna_nueva; */
 -- AFTER/ FIRST
 -- la cadena de texto se define entre comillas simples/dobles
--- INSERT INTO  escuelas VALUES(1, 'Normal 1', 'Quilmes', 'Buenos Aires', 200);
+ /* INSERT INTO  escuelas VALUES(1, 'Normal 1', 'Quilmes', 'Buenos Aires', 200); */
 
 -- consultando los registros de la tabla escuelas
--- 
-
+--SELECT * FROM escuelas;
+--USE db_23679;
 -- modificando registros
--- UPDATE escuelas SET localidad = 'Jujuy' WHERE id_escuela = 1;
+--UPDATE escuelas SET localidad = 'Jujuy' WHERE id_escuela = 1;
 
+--SELECT * FROM escuelas;
+--USE db_23679;
 -- insertando registros especificando las columnas 
--- INSERT INTO  escuelas (nombre, provincia, capacidad, localidad) VALUES('EET Nro 2', 'Santa Cruz', 500, 'CALAFATE');
+--INSERT INTO  escuelas (nombre, provincia, capacidad, localidad) VALUES('EET Nro 2', 'Santa Cruz', 500, 'CALAFATE');
 
--- INSERT INTO  escuelas (nombre, provincia, capacidad, localidad) VALUES('EET Nro 2', 'Tierra del fuego', 500, 'Rio Grande');
+--SELECT * FROM escuelas;
 
+ --INSERT INTO  escuelas (nombre, provincia, capacidad, localidad) VALUES('EET Nro 2', 'Tierra del fuego', 500, 'Rio Grande');
+--USE db_23679;
 -- modificar varias columnas
--- UPDATE escuelas SET id_escuela = 2, capacidad = 200 WHERE id_escuela = 0; 
-
+--UPDATE escuelas SET id_escuela = 2, capacidad = 200 WHERE id_escuela = 0; 
+--SELECT * FROM escuelas;
 -- seleccionado todas las columnas 
 -- SELECT * FROM escuelas;
-
+--USE db_23679;
 -- seleccionamos las columnas nombre, capacidad
--- SELECT nombre, capacidad, id_escuela FROM escuelas;
-
+--SELECT nombre, capacidad, id_escuela FROM escuelas;
+--USE db_23679;
 -- creando tabla alumnos
-/* CREATE TABLE alumnos (
+CREATE TABLE alumnos (
     id_alumno INT,
     escuela_id INT,
     nombre VARCHAR(30),
@@ -65,9 +69,9 @@ CREATE TABLE escuelas(
     PRIMARY KEY(id_alumno),
     FOREIGN KEY(escuela_id) REFERENCES escuelas(id_escuela)
 );
-*/
 
 
+USE db_23679;
 
 DESCRIBE alumnos;
 
